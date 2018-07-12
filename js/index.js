555443d47cfaf5bfe6db0bc6a86eb6230406e99e
Jon Oliver <imjonoliver@gmail.com>
2018-07-11 22:58:35 -0400

    Add start button

--- js/index.js
+++ js/index.js
@@ -34,6 +34,11 @@ const LetterSet = ({ droppableId, items }) => (
   </Droppable>
 )
 
+const StartButton = ({ started, onClick }) => (
+  started ? null :
+  <button id='start' onClick={onClick}>Play</button>
+);
+
 class Game extends Component {
   constructor(props){
     super(props);
@@ -44,8 +49,11 @@ class Game extends Component {
     this.state = {
       trayLetters,
       boardLetters,
+      started: false,
     }
+
     this.onDragEnd = this.onDragEnd.bind(this);
+    this.startGame = this.startGame.bind(this);
   }
 
   onDragEnd(result) {
@@ -79,14 +87,24 @@ class Game extends Component {
     }
   }
 
+  startGame(){
+    this.setState({
+      started: true,
+      trayLetters: getRandomLetters(),
+      boardLetters: [],
+    });
+    setTimeout(() => this.setState({ started: false }), 1500);
+  }
+
   render () {
-    const { trayLetters, boardLetters } = this.state;
+    const { trayLetters, boardLetters, started } = this.state;
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
         <LetterSet droppableId="trayLetters" items={trayLetters} />
         <div id="guess">
           <LetterSet droppableId="boardLetters" items={boardLetters} />
         </div>
+        <StartButton started={started} onClick={this.startGame} />
       </DragDropContext>
     );
   }
