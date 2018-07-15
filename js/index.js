2226e952520b52a07ae1a66e08e8bbadb95203b1
Jon Oliver <imjonoliver@gmail.com>
2018-07-14 21:30:40 -0400

    bold guessed word

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ const GameTime = ({ started, time }) =>
   <div id="timer"><span>{time}</span></div>;
 
 const WordNotification = ({ word, score }) =>
-  <div id="notify-is-word" className="">{word} is worth {score} points!</div>;
+  <div id="notify-is-word" className=""><b>{word}</b> is worth {score} points!</div>;
 
 const Score = ({ message, noWordMessage, word, score }) => {
   if (!word) return <div>{noWordMessage}</div>;
