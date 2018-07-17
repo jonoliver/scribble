47079851705bc577b7cce1e338d86fe7b019796d
Jon Oliver <imjonoliver@gmail.com>
2018-07-16 23:14:28 -0400

    Update test

--- js/index.spec.js
+++ js/index.spec.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Main from '.';
+import Game from '.';
 import renderer from 'react-test-renderer';
 
-it('displays the message (snapshot)', () => {
+const worker = {
+  onMessage: () => {},
+  postMessage: () => {},
+}
+
+it('renders', () => {
   const tree = renderer
-    .create(<Main message="Hello World!" />)
+    .create(<Game {...{ worker }} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-it('displays the message (enzyme)', () => {
-  const main = mount(<Main message="Hello World!" />);
-  expect(main.find("p").text()).toBe("Hello World!")
 });
